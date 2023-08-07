337c64b2d0af21e8f0e46769d3eef503799113c8
misung.dev <misung.dev@gmail.com>
2023-08-08 01:27:13 +0900

    Solve: 두 수의 나눗셈

---
+++ level0/120806.js
@@ -0,0 +1,4 @@
+const solution = (num1, num2) => Math.floor((num1 / num2) * 1000);
+
+// const solution = (num1, num2) => Math.trunc((num1 / num2) * 1000);
+// 음수인 경우도 생각해서 소수점을 이하 수를 없애고 싶을때는 Math.trunc를 사용하는 것이 좋다.
