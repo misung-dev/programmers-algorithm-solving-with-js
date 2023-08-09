e93a95be1dc13ad2e0fe2034978625e29167f9de
misung.dev <misung.dev@gmail.com>
2023-08-10 01:43:30 +0900

    Solve: 배열의 평균값

---
+++ level0/120817.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+	let sum = 0;
+
+	for (let i = 0; i < numbers.length; i++) {
+		sum += numbers[i];
+	}
+
+	const average = sum / numbers.length;
+	return average.toFixed(1);
+}
