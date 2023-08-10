37f6493e637414d041be2f2370f9f1bc45140e21
misung.dev <misung.dev@gmail.com>
2023-08-10 21:43:31 +0900

    Solve: 배열 뒤집기

--- level0/120898.js
+++ level0/120898.js
@@ -1,3 +1,7 @@
-function solution(message) {
-	return message.length * 2;
+function solution(num_list) {
+	return num_list.reverse();
 }
+
+// function solution(num_list) {
+// 	return num_list.sort((a, b) => -1);
+// }
