63f6041faaa2ca7f6676965e7af00a3ea2242aa5
misung.dev <misung.dev@gmail.com>
2023-08-12 00:11:24 +0900

    Solve: 자릿수 더하기

--- level0/120906.js
+++ level0/120906.js
@@ -3,7 +3,14 @@ function solution(n) {
 	var nums = n.toString().split("");
 
 	for (var i = 0; i < nums.length; i++) {
-		sum += nums[i];
+		sum += parseInt(nums[i]);
 	}
 	return sum;
 }
+
+// function solution(n) {
+//   return n
+//     .toString()
+//     .split("")
+//     .reduce((acc, cur) => acc + Number(cur), 0);
+// }
