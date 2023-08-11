e8b29e67e37ae19415f297d5e209b441158ad292
misung.dev <misung.dev@gmail.com>
2023-08-11 18:50:51 +0900

    Solve: 피자 나눠 먹기 (3)

---
+++ level0/120816.js
@@ -0,0 +1,7 @@
+function solution(slice, n) {
+	return Math.ceil(n / slice);
+}
+
+// Math.ceil 은 소수값이 존재할 때 값을 올리는 역활을 하는 함수
+// Math.floor 는 소수값이 존재할 때 소수값을 버리는 역활을 하는 함수
+// Math.round 는 소수값에 따라 올리거나 버리는 역활을 하는 반올림 함수
