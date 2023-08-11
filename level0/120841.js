5aff5cb0a3b1643980b695a202cdbe25bed954e2
misung.dev <misung.dev@gmail.com>
2023-08-11 20:33:24 +0900

    Solve: 점의 위치 구하기

--- level0/120841.js
+++ level0/120841.js
@@ -1,16 +1,8 @@
 function solution(dot) {
-	var x = dot[0];
-	var y = dot[1];
-
-	if (x != 0 && y != 0) {
-		if (x > 0 && y > 0) {
-			return 1;
-		} else if (x < 0 && y > 0) {
-			return 2;
-		} else if (x < 0 && y < 0) {
-			return 3;
-		} else if (x > 0 && y < 0) {
-			return 4;
-		}
-	}
+	var answer = 0;
+	if (dot[0] > 0 && dot[1] > 0) answer = 1;
+	if (dot[0] < 0 && dot[1] > 0) answer = 2;
+	if (dot[0] < 0 && dot[1] < 0) answer = 3;
+	if (dot[0] > 0 && dot[1] < 0) answer = 4;
+	return answer;
 }
