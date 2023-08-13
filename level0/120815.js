ab6849cefbb793b5b098c6799690c87e38564180
misung.dev <misung.dev@gmail.com>
2023-08-14 00:53:00 +0900

    Solve: 옷가게 할인 받기

--- level0/120815.js
+++ level0/120815.js
@@ -1,7 +1,22 @@
-function solution(n) {
-	for (var p = 1; p <= 100; p++) {
-		if ((6 * p) % n === 0) {
-			return p;
-		}
+function solution(price) {
+	if (price >= 500000) {
+		price = price * 0.8;
+	} else if (price >= 300000) {
+		price = price * 0.9;
+	} else if (price >= 100000) {
+		price = price * 0.95;
 	}
+
+	return Math.floor(price);
 }
+
+// 삼항연산자 이용
+// function solution(price) {
+// 	return price >= 500000
+// 		? parseInt(price * 0.8)
+// 		: price >= 300000
+// 		? parseInt(price * 0.9)
+// 		: price >= 100000
+// 		? parseInt(price * 0.9)
+// 		: price;
+// }
