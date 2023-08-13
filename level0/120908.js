eee00c4464b98bc1af206c43027a862f8cceb929
misung.dev <misung.dev@gmail.com>
2023-08-14 01:10:59 +0900

    Solve: 문자열안에 문자열

---
+++ level0/120908.js
@@ -0,0 +1,11 @@
+// function solution(str1, str2) {
+// 	if (str1.indexOf(str2) !== -1) {
+// 		return 1;
+// 	} else {
+// 		return 2;
+// 	}
+// }
+
+function solution(str1, str2) {
+	return str1.includes(str2) ? 1 : 2;
+}
