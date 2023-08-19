5f17be2b4f8faeee6a3d7bd9287e8413054fb761
misung.dev <misung.dev@gmail.com>
2023-08-20 00:30:47 +0900

    Solve: 피자 나눠 먹기 (2)

--- level0/120815.js
+++ level0/120815.js
@@ -1,22 +1,7 @@
-function solution(price) {
-	if (price >= 500000) {
-		price = price * 0.8;
-	} else if (price >= 300000) {
-		price = price * 0.9;
-	} else if (price >= 100000) {
-		price = price * 0.95;
+function solution(n) {
+	for (var p = 1; p <= 100; p++) {
+		if ((6 * p) % n === 0) {
+			return p;
+		}
 	}
-
-	return Math.floor(price);
 }
-
-// 삼항연산자 이용
-// function solution(price) {
-// 	return price >= 500000
-// 		? parseInt(price * 0.8)
-// 		: price >= 300000
-// 		? parseInt(price * 0.9)
-// 		: price >= 100000
-// 		? parseInt(price * 0.9)
-// 		: price;
-// }
