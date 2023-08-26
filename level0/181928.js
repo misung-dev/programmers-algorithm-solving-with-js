644b7f3c1bf1c91c6025f5f36b98e78c6200c3b5
misung.dev <misung.dev@gmail.com>
2023-08-27 00:49:55 +0900

    Solve: 이어 붙인 수

---
+++ level0/181928.js
@@ -0,0 +1,14 @@
+// solution 1
+function solution(num_list) {
+	let even = "";
+	let odd = "";
+
+	for (i = 0; i < num_list.length; i++) {
+		if (num_list[i] % 2 === 0) {
+			even += num_list[i];
+		} else {
+			odd += num_list[i];
+		}
+	}
+	return parseInt(even) + parseInt(odd);
+}
