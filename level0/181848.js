9cb2d4bf4425c01740bab24ee51a1c1a6dd41cde
misung.dev <misung.dev@gmail.com>
2023-08-30 01:13:33 +0900

    Solve: 문자열을 정수로 변환하기

---
+++ level0/181848.js
@@ -0,0 +1,12 @@
+// solution 1
+function solution(n_str) {
+	return parseInt(n_str);
+}
+
+// solution 2
+const solution = Number;
+
+// solution 3
+function solution(n_str) {
+	return Number(n_str);
+}
