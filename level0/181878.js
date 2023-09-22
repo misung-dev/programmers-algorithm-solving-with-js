14858d3717312d4687362c83550a0fa0917de18b
misung.dev <misung.dev@gmail.com>
2023-09-22 22:17:21 +0900

    Solve: 원하는 문자열 찾기

--- level0/181878.js
+++ level0/181878.js
@@ -1,6 +1,12 @@
-function solution(myString, pat) {
+// solution1
+function solution1(myString, pat) {
 	myString = myString.toLowerCase();
 	pat = pat.toLowerCase();
 
 	return myString.includes(pat) ? 1 : 0;
+}
+
+// solution2
+function solution2(myString, pat) {
+	return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
 }
