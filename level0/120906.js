2fe5ee76464d73464cdbaed4657536c1a2a60d75
misung.dev <misung.dev@gmail.com>
2024-03-11 23:42:09 +0900

    Solve: 자릿수 더하기

--- level0/120906.js
+++ level0/120906.js
@@ -1,16 +1,22 @@
+// solution1
 function solution(n) {
-	var sum = 0;
-	var nums = n.toString().split("");
+	let sum = 0;
+	let nums = n.toString().split("");
 
-	for (var i = 0; i < nums.length; i++) {
+	for (let i = 0; i < nums.length; i++) {
 		sum += parseInt(nums[i]);
 	}
 	return sum;
 }
 
-// function solution(n) {
-//   return n
-//     .toString()
-//     .split("")
-//     .reduce((acc, cur) => acc + Number(cur), 0);
-// }
+// solution2
+function solution(n) {
+	return n
+		.toString()
+		.split("")
+		.reduce((acc, cur) => acc + Number(cur), 0);
+}
+
+// 테스트 케이스
+console.log(solution(1234)); // 10
+console.log(solution(930211)); // 16
