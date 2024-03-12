20d55655f463a7b5421232875fa5401926c91e31
misung.dev <misung.dev@gmail.com>
2024-03-12 21:31:16 +0900

    Solve2: 중앙값 구하기

--- level0/120811.js
+++ level0/120811.js
@@ -1,4 +1,20 @@
-function solution(array) {
-	var answer = array.sort((a, b) => a - b);
+// solution 1
+function solution1(array) {
+	let answer = array.sort((a, b) => a - b);
 	return answer[Math.floor(answer.length / 2)];
 }
+
+// solution 2
+function solution2(array) {
+	const newArray = array.sort((a, b) => a - b);
+	const mid_num = Math.floor(newArray.length / 2);
+
+	return newArray[mid_num];
+}
+
+// solution 3
+const solution = (array) => array.sort((a, b) => a - b).at(Math.floor(array.length / 2));
+
+// 테스트 케이스
+console.log(solution([1, 2, 7, 10, 11])); //	7
+console.log(solution([9, -1, 0])); //	0
