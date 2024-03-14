91dfde23c61e412e1e62405187216e86e500e9cd
misung.dev <misung.dev@gmail.com>
2024-03-14 17:24:04 +0900

    Solve2: 짝수는 싫어요

--- level0/120813.js
+++ level0/120813.js
@@ -1,8 +1,12 @@
 function solution(n) {
-	var arr = [];
+	let arr = [];
 
-	for (var i = 1; i <= n; i += 2) {
+	for (let i = 1; i <= n; i += 2) {
 		arr.push(i);
 	}
 	return arr;
 }
+
+// 테스트 케이스
+console.log(solution(10)); // [1, 3, 5, 7, 9]
+console.log(solution(15)); // [1, 3, 5, 7, 9, 11, 13, 15]
