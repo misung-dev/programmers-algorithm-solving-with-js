f4d9bc73fe0c592228666ed7929669ea38b9546b
misung.dev <misung.dev@gmail.com>
2024-10-19 00:14:22 +0900

    refactor: 모의고사

--- level1/42840.js
+++ level1/42840.js
@@ -2,20 +2,18 @@
 
 function solution(answers) {
   let getCorrectAnswerCountsList = [0, 0, 0];
-  const STUDENT_1 = [1, 2, 3, 4, 5];
-  const STUDENT_2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const STUDENT_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  const students = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
 
   for (let i = 0; i < answers.length; i++) {
-    if (STUDENT_1[i % STUDENT_1.length] === answers[i]) {
-      getCorrectAnswerCountsList[0] += 1;
-    }
-    if (STUDENT_2[i % STUDENT_2.length] === answers[i]) {
-      getCorrectAnswerCountsList[1] += 1;
-    }
-    if (STUDENT_3[i % STUDENT_3.length] === answers[i]) {
-      getCorrectAnswerCountsList[2] += 1;
-    }
+    students.forEach((student, index) => {
+      if (student[i % student.length] === answers[i]) {
+        getCorrectAnswerCountsList[index] += 1;
+      }
+    });
   }
 
   const maxScore = Math.max(...getCorrectAnswerCountsList);
