602eff9a1011d5e0b2a12424f2e851ca43e548ed
misung.dev <misung.dev@gmail.com>
2025-11-27 23:05:55 +0900

    solve: H-Index

--- level2/42747.js
+++ level2/42747.js
@@ -2,31 +2,62 @@
 
 // solution1
 function solution1(citations) {
-	let count = citations.length;
-	const sortedCitations = citations.sort((a, b) => b - a);
+  let count = citations.length;
+  const sortedCitations = citations.sort((a, b) => b - a);
 
-	for (let i = 0; i < sortedCitations.length; i++) {
-		if (sortedCitations[i] === i + 1 || sortedCitations.length === 1) {
-			return i + 1;
-		}
-		if (sortedCitations[i] <= i + 1) {
-			return i;
-		}
-	}
-	return count;
+  for (let i = 0; i < sortedCitations.length; i++) {
+    if (sortedCitations[i] === i + 1 || sortedCitations.length === 1) {
+      return i + 1;
+    }
+    if (sortedCitations[i] <= i + 1) {
+      return i;
+    }
+  }
+  return count;
 }
 
 // solution2
+function solution2(citations) {
+  citations.sort((a, b) => b - a);
+
+  for (let i = 0; i < citations.length; i++) {
+    if (citations[i] <= i) {
+      return i;
+    }
+  }
+
+  return citations.length;
+}
+
+// solution3
 function solution(citations) {
-	citations.sort((a, b) => b - a);
+  let arr = citations.sort((a, b) => b - a);
 
-	for (let i = 0; i < citations.length; i++) {
-		if (citations[i] <= i) {
-			return i;
-		}
-	}
+  // for문 로직이 곧 H-index 정의라서, 분기처리 필요 없음
+  // 대신 함수 가장 하단에 return 0 추가
+  // const allSame = arr.every((v) => v === arr[0]);
 
-	return citations.length;
+  // if (allSame) {
+  //   return Math.min(arr[0], arr.length);
+  // }
+
+  // 조건을 만족하는 h값 중 가장 큰 값을 찾기 위해 큰 수부터 순회
+  for (let i = arr.length; i > 0; i--) {
+    let h = i;
+    let count = 0;
+    arr.forEach((v) => {
+      if (v >= h) {
+        count++;
+      }
+    });
+
+    // arr에서 h이상의 값이 h개 이상이 되는지 확인
+    if (count >= h) {
+      return h;
+    }
+  }
+
+  return 0;
 }
 
 // 테스트 케이스
