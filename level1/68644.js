aff528f87bdf851e871445e216d986d3c6bdc107
misung.dev <misung.dev@gmail.com>
2025-11-29 00:53:54 +0900

    solve: 두 개 뽑아서 더하기

--- level1/68644.js
+++ level1/68644.js
@@ -1,16 +1,37 @@
+function solution1(numbers) {
+  let answer = [];
+
+  for (i = 0; i < numbers.length; i++) {
+    for (j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (answer.indexOf(sum) === -1) {
+        answer.push(sum);
+      }
+    }
+  }
+
+  answer.sort((a, b) => a - b);
+
+  return answer;
+}
+
 function solution(numbers) {
-	let answer = [];
+  let sumArr = [];
 
-	for (i = 0; i < numbers.length; i++) {
-		for (j = i + 1; j < numbers.length; j++) {
-			let sum = numbers[i] + numbers[j];
-			if (answer.indexOf(sum) === -1) {
-				answer.push(sum);
-			}
-		}
-	}
+  // 덧셈 계산
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      sum = numbers[i] + numbers[j];
+      sumArr.push(sum);
+    }
+  }
 
-	answer.sort((a, b) => a - b);
+  // 중복된 숫자 제거
+  let uniqueArr = sumArr.filter((element, index) => {
+    return sumArr.indexOf(element) === index;
+  });
 
-	return answer;
+  uniqueArr.sort((a, b) => a - b);
+
+  return uniqueArr;
 }
